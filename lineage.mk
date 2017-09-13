cf3ceefbe241264c665f5778e209a9d2e00bbc51
Epirex <estebanguzzo@gmail.com>
2017-09-13 03:05:49 -0300

    Now we are Lineage OS!

--- lineage.mk
+++ lineage.mk
@@ -8,7 +8,7 @@ PRODUCT_RELEASE_NAME := GT-I8530
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage OS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -16,7 +16,7 @@ $(call inherit-product, device/samsung/gavini/full_gavini.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gavini
-PRODUCT_NAME := cm_gavini
+PRODUCT_NAME := lineage_gavini
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I8530
 PRODUCT_MANUFACTURER := samsung

Jtfk/android_device_samsung_janice

Now we are Lineage OS!

lineage.mk
@@ -8,7 +8,7 @@ PRODUCT_RELEASE_NAME := GT-I9070
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage OS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -22,7 +22,7 @@ PRODUCT_PACKAGES += \
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := janice
-PRODUCT_NAME := cm_janice
+PRODUCT_NAME := lineage_janice
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9070
 PRODUCT_MANUFACTURER := samsung

vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo cm_janice-eng
-add_lunch_combo cm_janice-userdebug
+add_lunch_combo lineage_janice-eng
+add_lunch_combo lineage_janice-userdebug

onioneq4/android_device_samsung_gavini

lineage.mk
@@ -8,7 +8,7 @@ PRODUCT_RELEASE_NAME := GT-I8530
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage OS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -16,7 +16,7 @@ $(call inherit-product, device/samsung/gavini/full_gavini.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gavini
-PRODUCT_NAME := cm_gavini
+PRODUCT_NAME := lineage_gavini
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I8530
 PRODUCT_MANUFACTURER := samsung

vendorsetup.sh
@@ -1,0 +1,2 @@
+add_lunch_combo lineage_gavini-eng
+add_lunch_combo lineage_gavini-userdebug