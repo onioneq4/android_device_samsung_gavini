898e6938a47ffe0054fd7b6aff9561a72b1a2657
onioneq4 <mailcloud@wp.pl>
2022-07-02 12:43:11 +0200

    use default toolchain

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,7 +6,6 @@ TARGET_SPECIFIC_HEADER_PATH := device/samsung/gavini/include
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/gavini
 TARGET_KERNEL_CONFIG := cyanogenmod_i8530_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/gavini/bluetooth
